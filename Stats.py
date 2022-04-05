357e6d00e47f2b50b0158c5c539221ace03b24d1
Jerome <DJerome99@laposte.net>
2022-04-05 10:52:53 +0200

    HomInde is fucked

--- Stats.py
+++ Stats.py
@@ -1,16 +1,41 @@
 from cmath import sqrt
 import statistics as stats
-import scipy.stats as st
+#import scipy.stats as st
 
 ############################################
-def EpsilonC(p, pe, n):
+def EpsilonCP(p, pe, n):
 
-    print(abs(p - pe)/sqrt((p*(1 - p)/n)))
-
-def EpsilonH(pe1, pe2, n1, p2, p):
+    print((abs(p - pe)/sqrt((p*(1 - p)/n))).real)
     
-    print(abs(pe1 - pe2)/sqrt( (p*(1 - p)/(1/n1 + 1/n2) ) ) )
+def EpsilonCSigma(xbar, mu, sigma, n):
+    
+    print((abs(xbar - mu)/(sigma/sqrt(n))).real)
 
+def EpsilonHP(pe1, pe2, n1, n2, p):
+    
+    print((abs(pe1 - pe2)/sqrt( (p*(1 - p)/(1/n1 + 1/n2) ) )).real )
+    
+def EpsilonHSigma(xbar1, xbar2, n1, n2, sig1, sig2):
+    
+    print((abs(xbar1 - xbar2)/sqrt( (sig1/n1 + sig2/n2) ) ).real )
+    
+def Comparison(p, f, q, n):
+    
+    print( abs(p - f)/(sqrt( (p*q/n) )) )
+    
+def ComparisonMulti(p1, p2, p, n1, n2):
+    
+    print( abs(p1 - p2)/(sqrt( p*(1-p)*(1/n1 + 1/n2) ) ) )
+
+#############################################
+def CI(xbar, n, s, z):
+    
+    print("Confidence Interval [", xbar - z*s/sqrt(n), ",", xbar + z*s/sqrt(n), "]")
+    
+def Z_Value(xbar, CI, s, n):
+    
+    print("Z = ", (xbar - CI)*sqrt(n)/s)
+    
 #############################################
 def Min(n2, pe2):
     
@@ -35,6 +60,7 @@ def mysum(a, b):
         
     return result
 
+#############################################
 def NormDis(ArrMean, SD):
     
     print(stats.NormalDist(ArrMean, SD))
@@ -46,52 +72,88 @@ def NormDisMan(N, Mean, X):
 #############################################
 def Conformity(Ti, Oi):
     result = 0
-    for t in Ti:
-        for o in Oi:
-            result += (t - o)**2 /t
+    for i in range(len(Ti)):
+        result += (Ti[i] - Oi[i])**2 /Ti[i]
     
     print(result)
 
+#############################################
+def Ei(N, Pi):
+    Ei = []
+
+    for i in range(len(Pi)):
+        Ei.append(N*Pi[i])
+        
+    print(Ei)
+
 ##############################################
-def HomInde(Oij):
+def HomInde(Oij, Rows):
         
     SumR = []
-    SumC = []
+    SumC = [0]*len(Oij[0])
+
+    result = 0;
 
     for r in Oij:
-        for i in range(Rows):
-            SumR.append(sum(r))
         for c in r:
-            for j in range(len(Oij[0])):
-                SumC.append(c)
-            
+            result += c
+        SumR.append(result)
+    Total = sum(SumR)
+    
+      
+    for r in Oij:
+        for i, c in enumerate(r):
+            SumC[i] += c
+        
     Tij = [[]*len(Oij[0])]*Rows
     
     
     for r in range(Rows):
         for c in range(len(Oij[0])):
-            Tij[r].append(SumR[r]*SumC[c]/sum(SumC))
+            Tij[r].append(SumR[r]*SumC[c]/Total)
             
     Chi = 0
     for r in range(Rows):
         for c in range(len(Oij[0])):
-            Chi += (Tij[r][c] - Oij[r][c])**2 /Tij[r][c]
+            Chi += (Oij[r][c] - Tij[r][c] )**2 /Tij[r][c]
             
     print(Chi)
-    
 ##############################################
-def SSquare(n, xarr, xarr2):
+def Adjustment(Ei, Oi):
+
+    result = 0
+
+    for i in range(len(Oi)):
+        result += ((Oi[i] - Ei[i])**2)/Ei[i]
+    
+    print(f"Chi² = {result}")
+##############################################
+def SSquare(n, xarr, xarr2, mu):
 
     xbar = xarr/n
 
     s2 = xarr2/n - xbar**2
 
-    print("s² = " + s2)
-    print(st.norm.ppf(s2))
-
-    return s2
+    print("s² = ", s2)
+    
+    t = (abs(xbar - mu)/(sqrt(s2/(n-1)))).real
+    
+    print("t = ", t)
 
 ##############################################
+def Fisher(n1, s1, n2, s2):
+    ec1 = (n1*s1**2)/(n1 - 1)
+    ec2 = (n2*s2**2)/(n2 - 1)
+    
+    print(ec2/ec1)
+    
+##############################################
+def Testn(xbar1, xbar2, ss, n1, n2):
+    
+    print( abs(xbar1 - xbar2)/(sqrt(ss(1/n1 + 1/n2))) )
+    
+def ss(n1, s1, s2, n2):
+    return (n1*s1**2 + n2*s2**2)/(n1+n2-2)
     
 ##################################################################################################################################
 
@@ -100,36 +162,71 @@ form = int(input())
 
 #Epsilon Conformity
 if form == 1 :
-    print("p = ")
-    p = float(input())
-    print("n = ")
-    n = float(input())
-    print("pe = (if you don't know type in 0)")
-    pe = float(input())
-    if pe == 0:
-        print("missing values? Lemme help..")
-        pe = float(input())/n
+    
+    print("With p(1) or Stadard deviation(2)?")
+    if int(input()) == 1:
+        
+        print("p = ")
+        p = float(input())
+        print("n = ")
+        n = float(input())
+        print("pe = (if you don't know type in 0)")
+        pe = float(input())
+        if pe == 0:
+            print("missing values? Lemme help..")
+            pe = float(input())/n
 
-    EpsilonC(p, pe, n)
+        EpsilonCP(p, pe, n)
+        
+    else:
+        print("xbar =")
+        xbar = float(input())
+        print("mu = ")
+        mu = float(input())
+        print("sigma =")
+        sigma = float(input())
+        print("n = ")
+        n = int(input())
+        
+        EpsilonCSigma(xbar, mu, sigma, n)
     
 #Epsilon Homogenity
 if form == 2:
     
-    print("pe1 = ")
-    pe1 = float(input())
-    print("pe2 = ")
-    pe2 = float(input())
-    print("n1 = ")
-    n1 = float(input())
-    print("n2 = ")
-    n2 = float(input())
-    print("p =  (if you don't know type in 0)")
-    p = float(input())
-    if p == 0:
-        print("missing values? Lemme help..")
-        p = float(input())/n
+    print("With p(1) or Stadard deviation(2)?")
+    if int(input()) == 1:
+    
+        print("pe1 = ")
+        pe1 = float(input())
+        print("pe2 = ")
+        pe2 = float(input())
+        print("n1 = ")
+        n1 = float(input())
+        print("n2 = ")
+        n2 = float(input())
+        print("p =  (if you don't know type in 0)")
+        p = float(input())
+        if p == 0:
+            print("missing values? Lemme help..")
+            p = float(input())/n
 
-    EpsilonH(pe1, pe2, n1, n2, p)
+        EpsilonHP(pe1, pe2, n1, n2, p)
+    
+    else:
+        print("X bar 1 = ")
+        xbar1 = float(input())
+        print("X bar 2 = ")
+        xbar2 = float(input())
+        print("n1 = ")
+        n1 = float(input())
+        print("n2 = ")
+        n2 = float(input())
+        print("Sigma 1 = ")
+        sig1 = float(input())
+        print("Sigma 2 = ")
+        sig2 = float(input())
+        
+        EpsilonHSigma(xbar1, xbar2, n1, n2, sig1, sig2)
 
 #Min
 if form == 3 :
@@ -240,7 +337,7 @@ if form == 7:
             
             else :
                 i+=1
-    HomInde(Oij)
+    HomInde(Oij, Rows)
 
 #SSquare
 if form == 8:
@@ -250,5 +347,158 @@ if form == 8:
     xarr = float(input())
     print("Sum xi² =")
     xarr2 = float(input())
+    print("mu =")
+    mu = float(input())
 
-    SSquare(n, xarr, xarr2)
+    SSquare(n, xarr, xarr2, mu)
+    
+#Test de Fisher
+if form == 9:
+    print("n1 =")
+    n1 = int(input())
+    print("s1 =")
+    s1 = float(input())
+    print("n2 =")
+    n2 = int(input())
+    print("s2 =")
+    s2 = float(input())
+    
+    Fisher(n1, s1, n2, s2)
+    
+#Test n<30
+if form == 10:
+    
+    print("xbar1 =")
+    xbar1 = float(input())
+    print("xbar2 =")
+    xbar2 = float(input())
+    print("n1 =")
+    n1 = int(input())
+    print("n2 =")
+    n2 = int(input())
+    
+    print("s1 =")
+    s1 = float(input())
+    print("n2 =")
+    s2 = float(input())
+    ss = ss(n1, s1, n2, s2)
+    
+    Testn(xbar1, xbar2, ss, n1, n2)
+    
+#Comparison
+if form == 11:
+    
+    print("p =")
+    p = float(input())
+    print("f =")
+    f = float(input())
+    print("q =")
+    q = float(input())
+    print("n =")
+    n = float(input())
+    
+    
+    Comparison(p, f, q, n)
+
+#Comparison Multiple Population
+if form == 12:
+    
+    print("p1 =")
+    p1 = float(input())
+    print("p2 =")
+    p2 = float(input())
+    print("p =")
+    p = float(input())
+    print("n1 =")
+    n1 = float(input())
+    print("n2 =")
+    n2 = float(input())
+    
+    ComparisonMulti(p1, p2, p, n1, n2)
+    
+#Confidence Intervals
+if form == 13:
+    
+    print("sample mean =")
+    xbar = float(input())
+    print("sample size =")
+    n = int(input())
+    print("sample standard deviation = ")
+    s = float(input())
+    print("confidence level? (0-9) 50% 60% 70% 80% 90% 95% 98% 99% 99.8% 99.9%")
+    z = [0.674, 0.842, 1.036, 1.282, 1.645, 1.960, 2.326, 2.576, 3.090, 3.291,]
+    i = int(input())
+    
+    CI(xbar, n, s, z[i])
+    
+#Z_Value
+if form == 14:
+    
+    print("sample mean =")
+    xbar = float(input())
+    print("sample size =")
+    n = int(input())
+    print("sample standard deviation = ")
+    s = float(input())
+    print("Confidance interval (smallest value)")
+    CI = float(input())
+    
+    Z_Value(xbar, CI, s, n)
+    print("Look at the Z-Value table for the confidence interval")
+    
+#Adjustment Chi
+if form == 15:
+
+    print("Expected values (-1when done)")
+    
+    Ei = []
+    
+    i = 0
+    while True :
+        Ei.append(float(input()))
+        
+        if Ei[i] < 0 :
+            Ei.pop()
+            break;
+        
+        else :
+            i+=1
+
+    print("Observed values (-1when done)")
+    
+    Oi = []
+    
+    i = 0
+    while True :
+        Oi.append(float(input()))
+        
+        if Oi[i] < 0 :
+            Oi.pop()
+            break;
+        
+        else :
+            i+=1
+
+    Adjustment(Ei, Oi)
+
+#Ei
+if form == 16:
+    
+    print("N = ")
+    n = int(input())
+    print("Pi values as decimal (-1when done)")
+    
+    Pi = []
+    
+    i = 0
+    while True :
+        Pi.append(float(input()))
+        
+        if Pi[i] < 0 :
+            Pi.pop()
+            break;
+        
+        else :
+            i+=1
+    
+    Ei(n, Pi)
